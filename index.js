3a01894eef491651e6af215a4f56708d4a1f3d5c
Makoy <neimarkbraga@gmail.com>
2021-04-14 16:28:55 +0800

    test change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const config = (() => {
 
 app.get('**', (req, res) => {
   res.json({
-    message: 'Hello World! [0]',
+    message: 'Hello World! [1]',
     APP_HOST: process.env.APP_HOST
   });
 });
